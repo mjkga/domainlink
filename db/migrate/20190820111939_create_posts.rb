4272cba1acde9ba2335ac3f29b9e1963785d1d24
Ubuntu <ubuntu@ip-172-31-3-116.us-east-2.compute.internal>
2019-08-20 11:52:32 +0000

    version 2

--- db/migrate/20190820111939_create_posts.rb
+++ db/migrate/20190820111939_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :name
+      t.string :content
 
       t.timestamps
     end
